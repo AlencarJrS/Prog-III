efaf4ac169ae79fbdba36a4874f4def550de0f3b
Alencar Junior <alencarjunior75@gmail.com>
2020-06-26 00:43:48 -0300

    FuncionEmail

---
+++ email.php
@@ -0,0 +1,21 @@
+<?php
+
+ini_set('display_errors', 1);
+
+error_reporting(E_ALL);
+
+$from = "alencar@alencarjunior.tech";
+
+$to = $_POST['alencarjunior75@gmail.com'];
+
+$subject = "Verificando o correio do PHP";
+
+$message = "O correio do PHP funciona bem";
+
+$headers = "De: ". $from;
+echo "teste";
+mail($to, $subject, $message, $headers);
+
+echo "A mensagem de e-mail foi enviada.";
+
+?>
